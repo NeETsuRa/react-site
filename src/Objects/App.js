f0b3b1853c83e629a0f5a3fa59dd0afb34ab3260
Nejc <dev.neetsu@gmail.com>
2019-03-18 00:51:45 +0100

    Add all of Photos

--- src/Objects/App.js
+++ src/Objects/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
-import logo from '../Media//logo.svg';
-import '../scss/App.scss';
 import Headder from './Headder';
+import Footer from './Footer';
+import Menu from './Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Headder></Headder>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Headder/>
+      <Menu/>
+      <Footer/>
       </div>
     );
   }
