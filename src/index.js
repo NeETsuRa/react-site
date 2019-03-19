441b4b69496b7f7de83635ec55b61fa5d3eb9e16
NeETsuRa <dev.neetsu@gmail.com>
2019-03-19 09:32:20 +0100

    update scss import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Main from "./Main";
-import "./index.scss";
+import Main from "./Objects/Main";
+import "./scss/index.scss";
 
 ReactDOM.render(
     <Main/>,
