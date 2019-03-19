1f3f323c5cfe42c579a664b409be40399f1487d3
Nejc <dev.neetsu@gmail.com>
2019-03-20 00:58:14 +0100

    Update scss structure and parameters

--- src/Objects/Home.js
+++ src/Objects/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="Home">
         <div className="MainHeader">        
           <h1>OBDELAVA KOVIN TOMO RAVNJAK S.P.</h1>
         </div>
