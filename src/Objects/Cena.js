7220c3e73fc8a1dc44337ee62142b1e076413796
Nejc <dev.neetsu@gmail.com>
2019-03-21 01:03:48 +0100

    Update site

--- src/Objects/Cena.js
+++ src/Objects/Cena.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class Cena extends Component {
   render() {
     return (
-      <div>
-        <h2>Cena</h2>
+      <div className="Home">
+        <div>Urna Predpostavka:</div>
       </div>
     );
   }
--- src/Objects/Kontakt.js
+++ src/Objects/Kontakt.js
@@ -4,7 +4,30 @@ class Kontakt extends Component {
   render() {
     return (
       <div>
-        <h2>Kontakt</h2>
+        <div className="Contact-Title">Kontaktni podatki</div>
+        <div>
+          <div>
+            <p className="Name">Tomo R.</p>
+            <p className="Number">031/811-633</p>
+          </div>
+          <div>
+            <p className="Name">Slavko R.</p>
+            <p className="Number">041/809-070</p>
+          </div>
+          <div>
+            <p className="Name">Mail</p>
+            <p className="Number Mail">tomo.ravnjak.s.p@gmail.com</p>
+          </div>
+        </div>
+        <div className="Contact-Title">Naslov</div>
+        <div>
+          <p className="Contact">Tomo ravnjak s.p</p>
+          <p className="Contact">Brezje pri Oplotnica 32</p>
+          <p className="Contact">2317 Oplotnica</p>
+        </div>
+        <div>
+          <iframe className="Map" title="GMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2752.0783695052564!2d15.429049415588636!3d46.38767067912273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47657e3b069deaeb%3A0x2a4be331e21e9d44!2sBrezje+pri+Oplotnici+32%2C+2317+Oplotnica%2C+Slovenia!5e0!3m2!1sen!2sat!4v1553124846786" ></iframe>
+        </div>
       </div>
     );
   }
