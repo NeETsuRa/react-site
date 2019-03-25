86e404b8d433a17d5a8b3f334291aba13362bfdf
Nejc <dev.neetsu@gmail.com>
2019-03-26 00:10:23 +0100

    List of mashines

--- src/Objects/Stroji.js
+++ src/Objects/Stroji.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import 
  
 class Stroji extends Component {
   render() {
     return (
       <div>
-        <h2>Stroji</h2>
+        <ul>
+          <li>WMW</li>
+          <li>Kikinda</li>
+          <li>Kikinda </li>
+          <li>Jung </li>
+          <li>Peskalna komora</li>
+          <li>Prvomajska ALG-200</li>
+          <li>Struženje </li>
+        </ul>
       </div>
     );
   }
