e3bec0b162e75583a9dd219202e6062c13790e84
Nejc <dev.neetsu@gmail.com>
2019-03-28 23:06:02 +0100

    Fix image component

--- src/Objects/Image.js
+++ src/Objects/Image.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
  
-class Image extends Component {
+class Image extends React.Component {
   render() {
     return (
-      <div>ssaass
+      <div>
         <div className="dropdown">
             <img src={this.props.img} className="mashinePhoto" alt="Photo" />
             <div className="dropdown-content">
@@ -15,5 +15,4 @@ class Image extends Component {
     );
   }
 }
- 
 export default Image;
--- src/Objects/Stroji.js
+++ src/Objects/Stroji.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import image from "./Image";
+import Image from "./Image";
 import jung from "../Media/stroji/20180130_182220.jpg";
 import WMW from "../Media/stroji/20180825_144002.jpg";
 import Kikinda from "../Media/stroji/20180825_143950.jpg";
@@ -7,7 +7,7 @@ import Kikinda2 from "../Media/stroji/20180825_143918.jpg";
 import Peskalna from "../Media/stroji/20180825_143729.jpg";
 import Prvomajska from "../Media/stroji/20180825_143750.jpg";
 import Struženje from "../Media/stroji/20180825_143831.jpg";
- 
+
 class Stroji extends Component {
   render() {
     return (
@@ -22,7 +22,7 @@ class Stroji extends Component {
               </div>
             </div> 
           </li>
-          <li>sas<image img={Kikinda} desc="txt"/></li>
+          <li><Image img={Kikinda} desc="txt"/></li>
           <li><img src={Kikinda2} className="mashinePhoto" alt="Kikinda" /> </li>
           <li><img src={jung} className="mashinePhoto" alt="Jung" /></li>
           <li><img src={Peskalna} className="mashinePhoto" alt="Peskalna komora" /></li>
