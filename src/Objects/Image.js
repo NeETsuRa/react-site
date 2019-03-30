5c51ebda917bf8c21b3ef41d622f75a8b60d42e4
Nejc <dev.neetsu@gmail.com>
2019-03-30 01:07:48 +0100

    Updafe popup box

--- src/Objects/Image.js
+++ src/Objects/Image.js
@@ -7,8 +7,8 @@ class Image extends React.Component {
         <div className="dropdown">
             <img src={this.props.img} className="mashinepreshow" alt="Photo" />
             <div className="dropdown-content">
-            <img src={this.props.img} className="mashinePhoto" alt="Photo" />
-            <div className="desc">{this.props.desc}</div>
+                <img src={this.props.img} className="mashinePhoto" alt="Photo" />
+                <div className="desc">{this.props.desc}</div>
             </div>
         </div> 
       </div>
