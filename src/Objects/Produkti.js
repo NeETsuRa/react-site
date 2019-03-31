0f7a0ce6a64e9de3ce34ca2698983e66d9a00d0a
Nejc <dev.neetsu@gmail.com>
2019-04-01 00:13:00 +0200

    Product impl

--- src/Objects/Produkti.js
+++ src/Objects/Produkti.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
- 
+import Image from "./Image";
+import prod from "../Media/produkti/20180203_100307.jpg";
+
 class Produkti extends Component {
   render() {
     return (
       <div>
-        <h2>Produkti</h2>
+        <ul className="productList">
+          <li>
+            <Image
+              img={prod}
+              title="Prod"
+              desc={[
+                'inf1', 
+                'Inf2'
+              ]}
+            />
+          </li>
+        </ul>
       </div>
     );
   }
